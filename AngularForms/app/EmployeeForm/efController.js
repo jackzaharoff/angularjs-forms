699a7122f19c5a667f71da5baf92824a939d43a4
Jack Zaharoff <jackzaharoff@gmail.com>
2015-02-21 16:08:27 -0500

    Add two way binding to the form, and move options to the controller as an array.

--- AngularForms/app/EmployeeForm/efController.js
+++ AngularForms/app/EmployeeForm/efController.js
@@ -1,3 +1,4 @@
-﻿angularFormsApp.controller('efController', function($scope, efService) {
+﻿angularFormsApp.controller("efController", function ($scope, efService) {
+    $scope.departments = ["Engineering", "Marketing", "Finance", "Administration"];
     $scope.employee = efService.employee;
 })
