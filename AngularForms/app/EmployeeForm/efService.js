e3fb2c37142a08f0419d4f2ca2afa26333b3ec78
Jack Zaharoff <jackzaharoff@gmail.com>
2015-02-21 15:29:29 -0500

    Refactor form with Bootstrap classes

---
+++ AngularForms/app/EmployeeForm/efService.js
@@ -0,0 +1,13 @@
+﻿angularFormsApp.factory("efService", function() {
+    return {
+        employee: {
+            fullName: "John Doe",
+            notes: "The ideal employee. Just don't touch his red stapler.",
+            department: "Administration",
+            perkCar: true,
+            perkStock: false,
+            perkSixWeeks: true,
+            payrollType: "none"
+        }
+    }
+});
